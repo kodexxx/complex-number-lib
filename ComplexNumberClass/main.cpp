24dd696d4109efc57215a77452189646856e33da
nazar-codefresh <nazar@codefresh.io>
2020-04-08 23:16:07 +0300

    Initial Commit

---
+++ ComplexNumberClass/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ComplexNumberClass
+//
+//  Created by Nazar Rogozhuk on 08.04.2020.
+//  Copyright © 2020 Nazar Rogozhuk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
